799c6692b2174d3dc1b23d31dfc492f8a143e152
MaximDragosAndrei <andrei.maxim97@gmail.com>
2017-04-12 13:43:06 +0300

    Update sign_in.php

--- sign_in.php
+++ sign_in.php
@@ -1,6 +1,6 @@
 <?php
   mysql_query("INSERT INTO Members (surname,firstname,username,email,address,phone, password,ficitv, birthdate)
   VALUES('". $_POST['post_surname'] ."','". $_POST['post_firstname'] ."','". $_POST['post_username'] ."',
-  '". $_POST['post_email'] ."','". $_POST['post_addess'] ."','". $_POST['post_phone'] ."','". $_POST['post_password'] ."',0,
+ lower( '". $_POST['post_email'] ."'),'". $_POST['post_addess'] ."','". $_POST['post_phone'] ."','". $_POST['post_password'] ."',0,
   CAST('". $date ."' AS DATE))") or die(mysql_error());
 ?>
