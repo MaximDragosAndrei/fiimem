38ba48e0e9a1dddbf82d16a44979348d968622b6
MaximDragosAndrei <andrei.maxim97@gmail.com>
2017-04-12 18:30:52 +0300

    Update sign_in.php

--- sign_in.php
+++ sign_in.php
@@ -1,6 +1,12 @@
 <?php
+  $postSurname = $_POST['post_surname'];
+  $postFirstname = $_POST['post_firstname'];
+  $postUsername = $_POST['post_username'] ;
+  $postEmail = lower(  $_POST['post_email']);
+  $postAddress = $_POST['post_addess'];
+  $postPhone = $_POST['post_phone'] ;
+  $postPassword = hash("sha256",$_POST['post_password']);
   mysql_query("INSERT INTO Members (surname,firstname,username,email,address,phone, password,ficitv, birthdate)
-  VALUES('". $_POST['post_surname'] ."','". $_POST['post_firstname'] ."','". $_POST['post_username'] ."',
- lower( '". $_POST['post_email'] ."'),'". $_POST['post_addess'] ."','". $_POST['post_phone'] ."','". $_POST['post_password'] ."',0,
-  CAST('". $date ."' AS DATE))") or die(mysql_error());
+  VALUES($postSurname,$postFirstname,$postUsername, $postEmail,$postAddress,$postPhone,$postPassword,0,
+  CAST('". $_POST['post_birth_date'] ."' AS DATE))") or die(mysql_error());
 ?>
